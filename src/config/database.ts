3c6975b2f1605de4954ef2b989cfd30add9abb57
Luis Reyes <luisrafaelreyescaro1@gmail.com>
2026-08-24 14:05:23 -0500

    Feat: update in system

--- src/config/database.ts
+++
@@ -1,7 +0,0 @@
-import { Sequelize } from "sequelize";
-import "dotenv/config";
-
-export const port: Number = parseInt(process.env.PORT as string, 10);
-export const host: String = process.env.URL as string;
-
-// const sequelize = new Sequelize()
--- src/models/user.model.ts
+++
@@ -1,9 +0,0 @@
-/**
- * diseño entidad usuario
- */
-export interface User {
-  id: number,
-  name: string
-}
-
-export const database: Array<User> = [{ id: 1, name: "Juan" }];
